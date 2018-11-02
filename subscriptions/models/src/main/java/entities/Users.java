261eaba85ca1247ff4b81696b047b7b2c1680a5e
Maja Umek <mu2729@student.uni-lj.si>
2018-11-02 20:05:48 +0100

    users entity zares

--- subscriptions/models/src/main/java/entities/Users.java
+++ subscriptions/models/src/main/java/entities/Users.java
@@ -1,4 +1,26 @@
 package entities;
 
+import javax.persistence.*;
+
+@Entity(name = "users")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String mail;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @Column(name = "artist_id")
+    private int artistId;
 }
