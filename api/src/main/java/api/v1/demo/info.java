b3006d42f75e2fd64d9e8b92dd6489543ca82285
Gal Bumbar <gal.bumbar@gmail.com>
2018-11-26 17:27:31 +0100

    zmetki za demo

--- api/src/main/java/api/v1/demo/info.java
+++ api/src/main/java/api/v1/demo/info.java
@@ -1,4 +1,10 @@
 package api.v1.demo;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
+import javax.ws.rs.ApplicationPath;
+
+@RegisterService
+@ApplicationPath("/api/v1")
 public class info {
 }
