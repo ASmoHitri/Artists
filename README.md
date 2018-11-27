7811d5f1cb2e0702030fd18af3fad26357dcc384
Maja Umek <mu2729@student.uni-lj.si>
2018-11-27 15:22:28 +0100

    info endpoint comments cleanup

--- README.md
+++ README.md
@@ -1,21 +1,12 @@
 # Streaming  
 
-Primeri sporočil in zahtevkov, ki jih bo obdelovala aplikacija:  
-	??  
-	
-Kompleksnješi zahtevki (pri obdelavi sodelujejo vsaj tri mikrostoritve):  
-	??
-
-
-+ update Architecture.png
-
 run docker container:  
-    docker run -dit --name catalogs -p 8081:8081 mu2729/streaming-catalogs
+    docker run -dit --name catalogs -p 8080:8080 mu2729/streaming-catalogs
 
 
 Konfiguracija:  
  - konfiguracijske datoteke:  
    + api/src/main/java/config.yml  
  - okoljske spremenljivke:  
-    override environment, ime microservica, verzijo, base-url in port serverja, datasource connection url, username, password, max-pool-size  
+    override environment, ime microservica, verzijo, base-url in port serverja, datasource connection url, username, password, max-pool-size,... 
  - konfiguracijski strežnik: spreminjanje konfiguracije, ki se jo lahko določi z okoljskimi spr.
--- api/src/main/java/api/v1/demo/InfoResource.java
+++ api/src/main/java/api/v1/demo/InfoResource.java
@@ -1,15 +1,11 @@
 package api.v1.demo;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
 
 @ApplicationScoped
 @Path("/demo")
@@ -18,30 +14,6 @@ public class InfoResource {
     @GET
     @Path("/info")
     public Response getInfo() {
-//        JSONObject jsonObject = new JSONObject();
-//        ArrayList<String> clani = new ArrayList<String>();
-//        clani.add("mu2729");
-//        clani.add("gb7532");
-//        ArrayList<String> mikrostoritve = new ArrayList<String>();
-//        mikrostoritve.add("http://159.122.186.127:30836/api/v1");
-//        mikrostoritve.add("http://159.122.186.127:30729");
-//        ArrayList<String> github = new ArrayList<String>();
-//        github.add("https://github.com/ASmoHitri/Catalogs");
-//        github.add("https://github.com/ASmoHitri/Subscriptions");
-//        ArrayList<String> travis = new ArrayList<String>();
-//        travis.add("https://travis-ci.org/ASmoHitri/Catalogs");
-//        travis.add("https://travis-ci.org/ASmoHitri/Subscriptions");
-//        ArrayList<String> dockerhub = new ArrayList<String>();
-//        dockerhub.add("https://hub.docker.com/r/mu2729/streaming-catalogs/");
-//        dockerhub.add("https://hub.docker.com/r/mu2729/streaming-subscriptions/");
-//
-//        jsonObject.put("clani", new JSONArray(clani))
-//                .put("opis_projekta", "Najin projekt implementira aplikacijo za pretocenje glasbe.")
-//                .put("mikrostoritve", new JSONArray(mikrostoritve))
-//                .put("github", new JSONArray(github))
-//                .put("travis", new JSONArray(travis))
-//                .put("dockerhub", new JSONArray(dockerhub));
-//        return Response.ok(jsonObject.toString()).build();
 
         String demo = "{\n" +
                 "\"clani\":[\"mu2729\", \"gb7532\"],\n" +
