a2eccdc8ee5eb35323fb0ffba947a2e663a3c8fe
Maja Umek <mu2729@student.uni-lj.si>
2019-01-07 21:57:21 +0100

    semicolons

--- models/src/main/resources/init-catalogs.sql
+++ models/src/main/resources/init-catalogs.sql
@@ -16,18 +16,18 @@ INSERT INTO albums(name, artist_id, genre_id) VALUES ('Moj zadnji album, ampak r
 INSERT INTO albums(name, artist_id, genre_id) VALUES ('Same ta dobre od KPopa', 4, 3);
 INSERT INTO albums(name, artist_id, genre_id) VALUES ('The Colour and the Shape', 5, 2);
 
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Still no snow', 1, 1, 2)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('And there was snow', 1, 1, 5)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Sploh ne rabm snega', 1, 1, 2)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Kaj ti bo sneg če imaš kolo', 1, 1, 2)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Krivo je more', 1, 1, 2)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Smells like global warming', 1, 1, 2)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Oh sweet Kentucky', 2, 2, 5)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Oh le kdaj se bom upokojil', 3, 3, 3)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Akonichisong-san', 4, 4, 4)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Everlong', 5, 5, 2)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('My Hero', 5 ,5, 2)
-INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Empire state of mind', 2, 2, 4)
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Still no snow', 1, 1, 2);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('And there was snow', 1, 1, 5);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Sploh ne rabm snega', 1, 1, 2);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Kaj ti bo sneg če imaš kolo', 1, 1, 2);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Krivo je more', 1, 1, 2);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Smells like global warming', 1, 1, 2);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Oh sweet Kentucky', 2, 2, 5);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Oh le kdaj se bom upokojil', 3, 3, 3);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Akonichisong-san', 4, 4, 4);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Everlong', 5, 5, 2);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('My Hero', 5 ,5, 2);
+INSERT INTO songs(title, artist_id, album_id, genre_id) VALUES ('Empire state of mind', 2, 2, 4);
 
 INSERT INTO playlists(name, user_id) VALUES ('Ta dobre', 1);
 INSERT INTO playlists(name, user_id) VALUES ('Ta slabe', 1);
